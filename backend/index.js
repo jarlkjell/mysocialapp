7ed96ea4bf129dafef34ebd7661d9e81f1a40cc8
Saqib Bhatti <saqibbhatti.public@gmail.com>
2022-12-01 16:15:21 +0100

    Register Endpoint with MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { mongo, default: mongoose } = require('mongoose');
+const mongoose= require('mongoose');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 
 dotenv.config();
 
@@ -11,9 +12,9 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
 
-app.use(async(req,res,next)=> {
+app.use(async (req,res,next)=> {
     await mongoose.connect(MONGO_URI);
-
+    
     next();
 })
 
@@ -21,13 +22,16 @@ const userSchema = new mongoose.Schema({
     id: String,
     email:{
         type: String,
-        unique: true
+        required: [true, "Please provide an Email!"],
+        unique: true,
     },
     name: String,
-    password: String,
+    password: {
+        type: String,
+        required: [true, "Please provide a password!"],
+    },
     year: Date,
-    category: S
-    tring
+    category: String
 })
 const User  = mongoose.model('User',userSchema);
 
@@ -39,6 +43,29 @@ app.get('/',(req,res) => {
 
 app.get('/health-check',(req,res ) => {
     res.status(200).send("I perfectly work fine on Status 200");
+})
+
+app.post('/register', async (req,res) => {
+    try {
+        const encryptedPassword = await bcrypt.hash(req.body.password,10);
+        const newUser = await User.create({
+            id: req.body.id,
+            email: req.body.email,
+            password: encryptedPassword,
+            year: req.body.year,
+            category: req.body.category
+        })
+        newUser.save();
+        res.status(200).send("User successfully saved");
+        // try {
+        //     newUser.save();
+        //     res.status(200).send("User successfully saved");
+        // } catch (e) {
+        //     res.status(500).send("Failed to save User");
+        // }
+    } catch (e) {
+        res.status(400).send("User already exists");
+    }
 })
 
 app.listen(PORT, () => {
